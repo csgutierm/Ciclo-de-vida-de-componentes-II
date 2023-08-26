e5eaeaaf8d0a6581f124dbedcb124c172e5a0484
csgutierm <csgutier@gmail.com>
2023-08-25 21:18:36 -0400

    Boton compartir

--- app/src/main/java/com/desafiolatam/listadodetiendas/ui/StoreDetailActivity.kt
+++ app/src/main/java/com/desafiolatam/listadodetiendas/ui/StoreDetailActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.desafiolatam.listadodetiendas.R
@@ -48,5 +49,21 @@ class StoreDetailActivity : AppCompatActivity() {
                     .into(imageViewStore)
             }
         }
+
+        val buttonShare = findViewById<Button>(R.id.compatirButton)
+        buttonShare.setOnClickListener {
+            val storeName = store?.name ?: "" // Obtén el nombre de la tienda del objeto Store
+
+            if (storeName.isNotEmpty()) {
+                val sendIntent: Intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, "¡Mira esta tienda genial: $storeName!")
+                    type = "text/plain"
+                }
+                val shareIntent = Intent.createChooser(sendIntent, null)
+                startActivity(shareIntent)
+            }
+        }
+
     }
 }
